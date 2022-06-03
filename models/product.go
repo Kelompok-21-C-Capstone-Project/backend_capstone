43bcb468d35ccc5d5818ac1e4466676fefdbc6dc
ValerieJosephine <salindehoadeline@gmail.com>
2022-06-03 13:48:23 +0700

    feat : add product model

---
+++ models/product.go
@@ -0,0 +1,15 @@
+package models
+
+import "time"
+
+//> model tipe data
+type Product struct {
+	ProductId   int       `gorm : "primaryKey" json:"id"`
+	ProductName string    `json:"name"`
+	Description string    `json:"description"`
+	UpdatedAt   time.Time `json:"updated"`
+}
+
+func (Product) TableName() string {
+	return ("product")
+}
