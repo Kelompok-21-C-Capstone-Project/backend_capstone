f7ed916542a21a634349e4dc40b3e4414603a78b
Rafki <74223938+osd0081@users.noreply.github.com>
2022-06-27 15:39:36 +0700

    feat: added slug to product category and payment method

--- models/product_category.go
+++ models/product_category.go
@@ -5,6 +5,7 @@ import "github.com/google/uuid"
 type ProductCategory struct {
 	Id          uuid.UUID `gorm:"type:varchar(50);not null"`
 	Name        string    `gorm:"type:varchar(50);not null"`
+	Slug        string    `gorm:"type:varchar(50);not null"`
 	IsAvailable bool      `gorm:"default:true;not null"`
 	Description string    `gorm:"type:text"`
 }
