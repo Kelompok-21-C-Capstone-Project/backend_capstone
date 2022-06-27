d99bdd421133a710800cea114e5844f218be4fef
Rafki <74223938+osd0081@users.noreply.github.com>
2022-06-27 10:07:50 +0700

    feat: added barebone

---
+++ repository/productbrand/postgres.go
@@ -0,0 +1,36 @@
+package productbrand
+
+import (
+	"backend_capstone/models"
+
+	"gorm.io/gorm"
+)
+
+type PostgresRepository struct {
+	db *gorm.DB
+}
+
+func NewPostgresRepository(db *gorm.DB) *PostgresRepository {
+	return &PostgresRepository{
+		db: db,
+	}
+}
+
+func (repo *PostgresRepository) FindById(id string) (productBrand *models.ProductBrand, err error) {
+	return
+}
+func (repo *PostgresRepository) FindByQuery(key string, value interface{}) (productBrands *[]models.ProductBrand, err error) {
+	return
+}
+func (repo *PostgresRepository) FindAll() (productBrands *[]models.ProductBrand, err error) {
+	return
+}
+func (repo *PostgresRepository) Insert() (productBrand *models.ProductBrand, err error) {
+	return
+}
+func (repo *PostgresRepository) Update() (productBrand *models.ProductBrand, err error) {
+	return
+}
+func (repo *PostgresRepository) Delete() (productBrand *models.ProductBrand, err error) {
+	return
+}
