0d4f1bdb38629dc3bf1e8a6a92b4e513f1bd40ff
Rafki <74223938+osd0081@users.noreply.github.com>
2022-06-30 08:23:16 +0700

    fix: repo func accept uuid as id

--- repository/productcategory/postgres.go
+++ repository/productcategory/postgres.go
@@ -17,9 +17,8 @@ func NewPostgresRepository(db *gorm.DB) *PostgresRepository {
 	}
 }
 
-func (repo *PostgresRepository) FindById(id string) (productCategory *models.ProductCategory, err error) {
-	uid, err := uuid.Parse(id)
-	if err = repo.db.First(&productCategory, uid).Error; err != nil {
+func (repo *PostgresRepository) FindById(id uuid.UUID) (productCategory *models.ProductCategory, err error) {
+	if err = repo.db.First(&productCategory, id).Error; err != nil {
 		return
 	}
 	return
@@ -39,16 +38,14 @@ func (repo *PostgresRepository) Insert(data *models.ProductCategory) (productCat
 	}
 	return data, err
 }
-func (repo *PostgresRepository) Update(id string, data *models.ProductCategory) (productCategory *models.ProductCategory, err error) {
-	uid, err := uuid.Parse(id)
-	if err = repo.db.First(&productCategory, uid).Model(productCategory).Updates(data).Error; err != nil {
+func (repo *PostgresRepository) Update(id uuid.UUID, data *models.ProductCategory) (productCategory *models.ProductCategory, err error) {
+	if err = repo.db.First(&productCategory, id).Model(productCategory).Updates(data).Error; err != nil {
 		return
 	}
 	return
 }
-func (repo *PostgresRepository) Delete(id string) (err error) {
-	uid, err := uuid.Parse(id)
-	if err = repo.db.Delete(&models.ProductCategory{}, uid).Error; err != nil {
+func (repo *PostgresRepository) Delete(id uuid.UUID) (err error) {
+	if err = repo.db.Delete(&models.ProductCategory{}, id).Error; err != nil {
 		return
 	}
 	return
