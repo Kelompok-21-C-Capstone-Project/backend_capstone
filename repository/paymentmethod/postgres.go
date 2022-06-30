c3b05ea6291c9867077aba33371ac597781f220b
Rafki <74223938+osd0081@users.noreply.github.com>
2022-06-30 23:19:01 +0700

    feat: payment method

--- repository/paymentmethod/postgres.go
+++ repository/paymentmethod/postgres.go
@@ -17,20 +17,35 @@ func NewPostgresRepository(db *gorm.DB) *PostgresRepository {
 }
 
 func (repo *PostgresRepository) FindById(id string) (paymentMethod *models.PaymentMethod, err error) {
+	if err = repo.db.Preload("PaymentVendors").First(&paymentMethod, id).Error; err != nil {
+		return
+	}
 	return
 }
 func (repo *PostgresRepository) FindByQuery(key string, value interface{}) (paymentMethods *[]models.PaymentMethod, err error) {
 	return
 }
 func (repo *PostgresRepository) FindAll() (paymentMethods *[]models.PaymentMethod, err error) {
+	if err = repo.db.Preload("PaymentVendors").Find(&paymentMethods).Error; err != nil {
+		return
+	}
 	return
 }
-func (repo *PostgresRepository) Insert() (paymentMethod *models.PaymentMethod, err error) {
+func (repo *PostgresRepository) Insert(data *models.PaymentMethod) (paymentMethod *models.PaymentMethod, err error) {
+	if err = repo.db.Create(data).Error; err != nil {
+		return
+	}
+	return data, err
+}
+func (repo *PostgresRepository) Update(id string, data *models.PaymentMethod) (paymentMethod *models.PaymentMethod, err error) {
+	if err = repo.db.First(&paymentMethod, id).Model(paymentMethod).Updates(data).Preload("PaymentVendors").First(&paymentMethod, id).Error; err != nil {
+		return
+	}
 	return
 }
-func (repo *PostgresRepository) Update() (paymentMethod *models.PaymentMethod, err error) {
-	return
-}
-func (repo *PostgresRepository) Delete() (paymentMethod *models.PaymentMethod, err error) {
+func (repo *PostgresRepository) Delete(id string) (err error) {
+	if err = repo.db.Delete(&models.PaymentMethod{}, id).Error; err != nil {
+		return
+	}
 	return
 }
--- services/paymentmethod/service.go
+++ services/paymentmethod/service.go
@@ -2,25 +2,28 @@ package paymentmethod
 
 import (
 	"backend_capstone/models"
+	"backend_capstone/services/paymentmethod/dto"
+	"strings"
 
 	"github.com/go-playground/validator/v10"
+	"github.com/google/uuid"
 )
 
 type Repository interface {
 	FindById(id string) (paymentMethod *models.PaymentMethod, err error)
 	FindByQuery(key string, value interface{}) (paymentMethods *[]models.PaymentMethod, err error)
 	FindAll() (paymentMethods *[]models.PaymentMethod, err error)
-	Insert() (paymentMethod *models.PaymentMethod, err error)
-	Update() (paymentMethod *models.PaymentMethod, err error)
-	Delete() (paymentMethod *models.PaymentMethod, err error)
+	Insert(data *models.PaymentMethod) (paymentMethod *models.PaymentMethod, err error)
+	Update(id string, data *models.PaymentMethod) (paymentMethod *models.PaymentMethod, err error)
+	Delete(id string) (err error)
 }
 
 type Service interface {
 	GetById(id string) (paymentMethod models.PaymentMethod, err error)
 	GetAll() (paymentMethods []models.PaymentMethod, err error)
-	Create() (paymentMethod models.PaymentMethod, err error)
-	Modify() (paymentMethod models.PaymentMethod, err error)
-	Remove() (err error)
+	Create(methodDTO dto.CreateMethodDTO) (paymentMethod models.PaymentMethod, err error)
+	Modify(id string, methodDTO dto.CreateMethodDTO) (paymentMethod models.PaymentMethod, err error)
+	Remove(id string) (err error)
 }
 
 type service struct {
@@ -36,17 +39,61 @@ func NewService(repository Repository) Service {
 }
 
 func (s *service) GetById(id string) (paymentMethod models.PaymentMethod, err error) {
+	_, err = uuid.Parse(id)
+	if err != nil {
+		return
+	}
+	data, err := s.repository.FindById(id)
+	if err != nil {
+		return
+	}
+	paymentMethod = *data
 	return
 }
 func (s *service) GetAll() (paymentMethods []models.PaymentMethod, err error) {
+	data, err := s.repository.FindAll()
+	if err != nil {
+		return
+	}
+	paymentMethods = *data
 	return
 }
-func (s *service) Create() (paymentMethod models.PaymentMethod, err error) {
+func (s *service) Create(methodDTO dto.CreateMethodDTO) (paymentMethod models.PaymentMethod, err error) {
+	if err = s.validate.Struct(methodDTO); err != nil {
+		return
+	}
+	id := uuid.New().String()
+	slug := strings.ReplaceAll(strings.ToLower(methodDTO.Name), " ", "-")
+	data, err := s.repository.Insert(methodDTO.GenerateModel(id, slug))
+	paymentMethod = *data
 	return
 }
-func (s *service) Modify() (paymentMethod models.PaymentMethod, err error) {
+func (s *service) Modify(id string, methodDTO dto.CreateMethodDTO) (paymentMethod models.PaymentMethod, err error) {
+	_, err = uuid.Parse(id)
+	if err != nil {
+		return
+	}
+	_, err = s.repository.FindById(id)
+	if err != nil {
+		return
+	}
+	slug := strings.ReplaceAll(strings.ToLower(methodDTO.Name), " ", "-")
+	data, err := s.repository.Insert(methodDTO.GenerateModel(id, slug))
+	paymentMethod = *data
 	return
 }
-func (s *service) Remove() (err error) {
+func (s *service) Remove(id string) (err error) {
+	_, err = uuid.Parse(id)
+	if err != nil {
+		return
+	}
+	_, err = s.repository.FindById(id)
+	if err != nil {
+		return
+	}
+	err = s.repository.Delete(id)
+	if err != nil {
+		return
+	}
 	return
 }
