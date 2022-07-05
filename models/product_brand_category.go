7e4201d2c8d6435a8b7cbaa1ddc29dd9e9a31b50
Rafki <74223938+osd0081@users.noreply.github.com>
2022-07-05 21:38:19 +0700

    fix: omit on is_available tag

--- models/product_brand_category.go
+++ models/product_brand_category.go
@@ -5,6 +5,6 @@ type ProductBrandCategory struct {
 	ProductBrandId    string    `gorm:"type:varchar(50);not null" json:"brand_id,omitempty" example:"xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"`
 	ProductCategoryId string    `gorm:"type:varchar(50);not null" json:"category_id,omitempty" example:"xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"`
 	Slug              string    `gorm:"type:varchar(50);not null" json:"slug,omitempty"`
-	IsAvailable       bool      `gorm:"default:true;not null" json:"is_available,omitempty" example:"true"`
+	IsAvailable       bool      `gorm:"default:true;not null" json:"is_available" example:"true"`
 	Products          []Product `json:"products,omitempty"`
 }
