acac6fa17d49c576ebca7fd3ba0ecd22ba86513b
Rafki <74223938+osd0081@users.noreply.github.com>
2022-07-05 11:14:30 +0700

    feat:factory for mysql

--- repository/user/factory.go
+++ repository/user/factory.go
@@ -10,6 +10,8 @@ func RepositoryFactory(dbCon *utils.DatabaseConnection) user.Repository {
 
 	if dbCon.Driver == utils.Postgres {
 		transactionsRepo = NewPostgresRepository(dbCon.Postgres)
+	} else if dbCon.Driver == utils.MySQL {
+		transactionsRepo = NewPostgresRepository(dbCon.Postgres)
 	}
 
 	return transactionsRepo
