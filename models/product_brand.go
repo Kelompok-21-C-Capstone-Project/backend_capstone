3a22a509128df9ae1dacb9165a95e9080d63f899
Rafki <74223938+osd0081@users.noreply.github.com>
2022-07-12 10:05:24 +0700

    fix: tag for brand

--- models/product_brand.go
+++ models/product_brand.go
@@ -1,10 +1,10 @@
 package models
 
 type ProductBrand struct {
-	Id                string            `gorm:"type:varchar(50);not null" example:"xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"`
-	Name              string            `gorm:"type:varchar(50);not null" example:"some name"`
-	IsAvailable       bool              `gorm:"default:true;not null" example:"false"`
-	Description       string            `gorm:"type:text" example:"some description"`
+	Id                string            `json:"id" gorm:"type:varchar(50);not null" example:"xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"`
+	Name              string            `json:"name" gorm:"type:varchar(50);not null" example:"some name"`
+	IsAvailable       bool              `json:"is_available" gorm:"default:true;not null" example:"false"`
+	Description       string            `json:"description" gorm:"type:text" example:"some description"`
 	IconPath          string            `gorm:"type:text" example:"/path/to/file" json:"icon_path,omitempty"`
 	ProductCategories []ProductCategory `gorm:"many2many:brand_categories" json:"product_category,omitempty"`
 }
