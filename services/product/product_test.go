e1d97b2ae5e473e37e5640a1f145d50e8d951d6c
Rafki <74223938+osd0081@users.noreply.github.com>
2022-07-14 12:25:04 +0700

    fix: conflict

--- services/product/product_test.go
+++ services/product/product_test.go
@@ -3,6 +3,7 @@ package product_test
 import (
 	"backend_capstone/models"
 	"backend_capstone/services/product"
+	"backend_capstone/services/product/dto"
 	"backend_capstone/services/product/mocks"
 	"testing"
 
@@ -10,9 +11,8 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
-<<<<<<< HEAD
-var productrepo = &repository.ProductRepoMock{Mock: mock.Mock{}}
-var productservice = service{repository: productrepo}
+var productrepo = &mocks.Repository{Mock: mock.Mock{}}
+var productservice = product.NewService(productrepo)
 
 func TestProductService_GetById(t *testing.T) {
 
@@ -23,8 +23,8 @@ func TestProductService_GetById(t *testing.T) {
 
 	assert.Nil(t, product)
 	assert.NotNil(t, err)
+}
 
-=======
 func TestGetById(t *testing.T) {
 	var mockRepo = new(mocks.Repository)
 	t.Run("success", func(t *testing.T) {
@@ -33,7 +33,6 @@ func TestGetById(t *testing.T) {
 		_, err := service.GetById("5f8fe693-fbee-438d-9ca9-a58b7c0b6bf5")
 		assert.NoError(t, err)
 	})
->>>>>>> 33036eb8bf95600bbda7d61db48858178a315595
 }
 
 func TestProductService_GetAll(t *testing.T) {
@@ -53,7 +52,7 @@ func TestProductService_ModifyProduct(t *testing.T) {
 	// program mock
 	productrepo.Mock.On("Update", "1").Return(nil)
 
-	product, err := productservice.Modify()
+	product, err := productservice.Modify("x", dto.UpdateProductDTO{})
 
 	assert.Nil(t, product)
 	assert.NotNil(t, err)
@@ -65,7 +64,7 @@ func TestProductService_CreateProduct(t *testing.T) {
 	// program mock
 	productrepo.Mock.On("Create", "id=?", "name=?", "price=?", "stock=?", "IsAvailable=?", "Description=?").Return(nil)
 
-	product, err := productservice.Create()
+	product, err := productservice.Create(dto.CraeteProductDTO{})
 
 	assert.Nil(t, product)
 	assert.NotNil(t, err)
@@ -77,9 +76,8 @@ func TestProductService_RemoveProduct(t *testing.T) {
 	// program mock
 	productrepo.Mock.On("Delete", "id=?").Return(nil)
 
-	product, err := productservice.Remove()
+	err := productservice.Remove("x")
 
-	assert.Nil(t, product)
 	assert.NotNil(t, err)
 
 }
